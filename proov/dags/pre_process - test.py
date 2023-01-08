9bcbbe83e5a72c0a9cb1e13a3ea0a398b2c59dc1
taunotamm <tauno.tammm@gmail.com>
2023-01-08 15:06:22 +0200

    Update pre_process - test.py

--- proov/dags/pre_process - test.py
+++ proov/dags/pre_process - test.py
@@ -4,9 +4,18 @@ import pandas as pd
 
 zf = zipfile.ZipFile('../data/archive.zip') 
 df = pd.read_json(zf.open('arxiv-metadata-oai-snapshot.json'), lines=True, chunksize=50000)
+
 counter = 0
+df_minimized = pd.DataFrame()
 for chunk in df:
     counter += 1
-    print(chunk)
+    #print(chunk)
+    df_minimized = pd.concat([df_minimized, chunk])
     if (counter == 4):
         break
+#df_minimized.drop_duplicates()
+df_minimized = df_minimized.drop(columns=['abstract'])
+df_minimized = df_minimized[df_minimized.authors != None]
+#df_minimized = df_minimized[df_minimized.journal-ref != 'None']
+#print(df_minimized)
+print(df_minimized.head())
